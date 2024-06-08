50531921275345fb16cc00baaaecf752bd673e2a
Harvey Yerik <yerik05vh@gmail.com>
2024-06-07 22:39:09 -0500

    :tada: Start project

---
+++ server/src/data/products.js
@@ -0,0 +1,12 @@
+const products = [
+  {
+    id: "trj-crd",
+    name: "Tarjeta de Crédito",
+    description: "Tarjeta de consumo bajo la modalidad de crédito",
+    logo: "https://www.visa.com.ec/dam/VCOM/regional/lac/SPA/Default/Pay%20With%20Visa/Tarjetas/visa-signature-400x225.jpg",
+    date_release: "2023-02-01",
+    date_revision: "2024-02-01",
+  },
+];
+
+module.exports = products;
